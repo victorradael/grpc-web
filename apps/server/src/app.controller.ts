ff64b1573624595aa8465a90413b643e3a0176e1
victorradael <victorradael+github@gmail.com>
2022-11-07 22:40:50 -0300

    New logger

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Metadata } from '@grpc/grpc-js';
+import { Metadata, ServerUnaryCall } from '@grpc/grpc-js';
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   @GrpcMethod('TemplateService', 'TemplateFunction')
-  TemplateFunction(data, metadata: Metadata) {
-    console.log(metadata);
+  TemplateFunction(
+    data,
+    metadata: Metadata,
+    serverUnaryCall: ServerUnaryCall<any, any>,
+  ) {
+    // console.log(metadata.get('user-agent'));
+    // console.log(metadata.getMap());
+    // console.log(metadata.toHttp2Headers());
+    // console.log(metadata.toJSON());
+    // console.log(serverUnaryCall.addListener('GT', () => 'D'));
+    // console.log(serverUnaryCall.getPeer());
+    // console.log(serverUnaryCall.getPath());
+    // console.log(serverUnaryCall.getDeadline());
+
+    const logBody = {
+      peer: serverUnaryCall.getPeer(),
+      path: serverUnaryCall.getPath(),
+      deadline: serverUnaryCall.getDeadline(),
+      metadata: metadata.toHttp2Headers(),
+    };
+
+    console.log(logBody);
 
     return { message: 'OK' };
   }
